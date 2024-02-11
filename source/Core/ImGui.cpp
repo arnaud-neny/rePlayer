85bb58c80be2cd0e00a9a684a1c4c96ab23d2e40
replay <mr.arnal@gmail.com>
2024-02-11 18:48:23 +0000

    imgui: add spinner widget

--- source/Core/ImGui.cpp
+++ source/Core/ImGui.cpp
@@ -1,3 +1,6 @@
+#include <Containers/Array.h>
+#include <ImGui/imgui_internal.h>
+
 #include "Imgui.h"
 
 namespace ImGui
@@ -15,12 +18,58 @@ namespace ImGui
 
     bool InputText(const char* label, std::string* str, ImGuiInputTextFlags flags)
     {
-        return ImGui::InputText(label, str->data(), str->size() + 1, ImGuiInputTextFlags_CallbackResize | flags, s_resizeCallback, str);
+        return InputText(label, str->data(), str->size() + 1, ImGuiInputTextFlags_CallbackResize | flags, s_resizeCallback, str);
     }
 
     bool InputTextMultiline(const char* label, std::string* str, const ImVec2& size, ImGuiInputTextFlags flags)
     {
-        return ImGui::InputTextMultiline(label, str->data(), str->size() + 1, size, ImGuiInputTextFlags_CallbackResize | flags, s_resizeCallback, str);
+        return InputTextMultiline(label, str->data(), str->size() + 1, size, ImGuiInputTextFlags_CallbackResize | flags, s_resizeCallback, str);
+    }
+
+    struct BusyData
+    {
+        ImVec2 startPos;
+        ImVec2 region;
+        bool isEnabled;
+    };
+    static core::Array<BusyData> s_busyStack;
+
+    void BeginBusy(bool isBusy)
+    {
+        s_busyStack.Add({ ImGui::GetCursorScreenPos(), ImGui::GetContentRegionAvail(), isBusy });
+    }
+
+    void BeginBusy(const ImVec2& screenPos, const ImVec2& region, bool isBusy)
+    {
+        s_busyStack.Add({ screenPos, region, isBusy });
+    }
+
+    void EndBusy(float time, float radius, float thickness, int32_t numSegments, float speed, uint32_t color)
+    {
+        const auto& busyData = s_busyStack.Last();
+        if (busyData.isEnabled)
+        {
+            ImGui::SameLine(0.0f, 0.0f);
+            const auto& region = busyData.region;
+            auto clampedRadius = core::Min(region.x * 0.5f, region.y * 0.5f, radius);
+            thickness *= clampedRadius / radius;
+
+            auto* drawList = GetForegroundDrawList();
+
+            auto scaledTime = time * speed;
+            drawList->PathClear();
+            auto start = int32_t(fabs(ImSin(scaledTime) * ((231 * numSegments) / 256)));
+            const float a_min = IM_PI * 2.0f * (float(start)) / float(numSegments);
+            const float a_max = IM_PI * 2.0f * (float((247 * numSegments) / 256)) / float(numSegments);
+            const auto centre = busyData.startPos + region * 0.5f;
+            for (auto i = 0; i < numSegments; i++)
+            {
+                const float a = a_min + (float(i) / float(numSegments)) * (a_max - a_min);
+                drawList->PathLineTo({ centre.x + ImCos(a + scaledTime * 8) * clampedRadius, centre.y + ImSin(a + scaledTime * 8) * clampedRadius });
+            }
+            drawList->PathStroke(color, false, thickness);
+        }
+        s_busyStack.Pop();
     }
 }
-//namespace ImGui
+// namespace ImGui
--- source/Core/ImGui.h
+++ source/Core/ImGui.h
@@ -15,19 +15,23 @@ namespace ImGui
     {
         if constexpr ((sizeof...(Arguments)) == 1)
         {
-            ImGui::BeginTooltip();
-            ImGui::TextUnformatted(std::forward<Arguments>(args)...);
-            ImGui::EndTooltip();
+            BeginTooltip();
+            TextUnformatted(std::forward<Arguments>(args)...);
+            EndTooltip();
         }
         else
         {
-            ImGui::SetTooltip(std::forward<Arguments>(args)...);
+            SetTooltip(std::forward<Arguments>(args)...);
         }
     }
 
     inline bool DragUint(const char* label, uint32_t* v, float v_speed = 1.0f, uint32_t v_min = 0, uint32_t v_max = 0, const char* format = "%d", ImGuiSliderFlags flags = 0, const ImVec2& text_align = ImVec2(0.5f, 0.5f))
     {
-        return ImGui::DragScalar(label, ImGuiDataType_U32, v, v_speed, &v_min, &v_max, format, flags, text_align);
+        return DragScalar(label, ImGuiDataType_U32, v, v_speed, &v_min, &v_max, format, flags, text_align);
     }
+
+    void BeginBusy(bool isBusy);
+    void BeginBusy(const ImVec2& screenPos, const ImVec2& region, bool isBusy);
+    void EndBusy(float time, float radius, float thickness, int32_t numSegments, float speed, uint32_t color);
 }
-//namespace ImGui
+// namespace ImGui
