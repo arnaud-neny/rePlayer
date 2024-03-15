eb4da6d9636a382c1f27fb504dc85db7c563c474
replay <mr.arnal@gmail.com>
2024-03-15 15:48:58 +0000

    libxml2: libxml 2.12.6

--- source/3rdParty/libxml2/include/libxml/xmlversion.h
+++ source/3rdParty/libxml2/include/libxml/xmlversion.h
@@ -29,21 +29,21 @@ XMLPUBFUN void xmlCheckVersion(int version);
  *
  * the version string like "1.2.3"
  */
-#define LIBXML_DOTTED_VERSION "2.12.5"
+#define LIBXML_DOTTED_VERSION "2.12.6"
 
 /**
  * LIBXML_VERSION:
  *
  * the version number: 1.2.3 value is 10203
  */
-#define LIBXML_VERSION 21205
+#define LIBXML_VERSION 21206
 
 /**
  * LIBXML_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "10203"
  */
-#define LIBXML_VERSION_STRING "21205"
+#define LIBXML_VERSION_STRING "21206"
 
 /**
  * LIBXML_VERSION_EXTRA:
@@ -58,7 +58,7 @@ XMLPUBFUN void xmlCheckVersion(int version);
  * Macro to check that the libxml version in use is compatible with
  * the version the software has been compiled against
  */
-#define LIBXML_TEST_VERSION xmlCheckVersion(21205);
+#define LIBXML_TEST_VERSION xmlCheckVersion(21206);
 
 #ifndef VMS
 #if 1//@WITH_TRIO@
@@ -387,7 +387,7 @@ XMLPUBFUN void xmlCheckVersion(int version);
  *
  * the string suffix used by dynamic modules (usually shared libraries)
  */
-#define LIBXML_MODULE_EXTENSION "@MODULE_EXTENSION@"
+#define LIBXML_MODULE_EXTENSION ""
 #endif
 
 /**
