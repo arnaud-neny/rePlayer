95c44db0d27b77450126b01ce15a63bb57cccb89
replay <mr.arnal@gmail.com>
2025-02-10 18:32:29 +0000

    goattracker: add advanced parameters

--- source/Replays/GoatTracker/ReplayGoatTracker.cpp
+++ source/Replays/GoatTracker/ReplayGoatTracker.cpp
@@ -1,6 +1,7 @@
 #include "ReplayGoatTracker.h"
 
 #include <Audio/AudioTypes.inl.h>
+#include <Core/Log.h>
 #include <Core/String.h>
 #include <Core/Window.inl.h>
 #include <Imgui.h>
@@ -51,8 +52,8 @@ namespace rePlayer
         unsigned ntsc = 0;
         unsigned sidaddress = 0xd400;
         unsigned finevibrato = 1;
-        unsigned optimizepulse = 1;
-        unsigned optimizerealtime = 1;
+        unsigned optimizepulse = 0;
+        unsigned optimizerealtime = 0;
         unsigned interpolate = 0;
         unsigned residdelay = 0;
 
@@ -64,6 +65,8 @@ namespace rePlayer
         char* programname;
 
         char textbuffer[MAX_PATHNAME];
+
+        extern int songinit;
 
         // stdio_wrap
         FILE* replayer_fopen(const char* filename, const char* mode)
@@ -150,6 +153,9 @@ namespace rePlayer
             window.RegisterSerializedData(ms_globals.isNtsc, "ReplayGoatTrackerNtsc");
             window.RegisterSerializedData(ms_globals.isFiltering, "ReplayGoatTrackerFiltering");
             window.RegisterSerializedData(ms_globals.isDistortion, "ReplayGoatTrackerDistortion");
+            window.RegisterSerializedData(ms_globals.fineVibrato, "ReplayGoatTrackerFineVibrato");
+            window.RegisterSerializedData(ms_globals.pulseOptimization, "ReplayGoatTrackerPulseOptim");
+            window.RegisterSerializedData(ms_globals.effectOptimization, "ReplayGoatTrackerEffectOptim");
             window.RegisterSerializedData(ms_globals.surround, "ReplayGoatTrackerSurround");
         }
 
@@ -158,6 +164,37 @@ namespace rePlayer
 
     Replay* ReplayGoatTracker::Load(io::Stream* stream, CommandBuffer metadata)
     {
+        auto globals = *static_cast<Globals*>(g_replayPlugin.globals);
+        auto* settings = metadata.Find<Settings>();
+        if (settings)
+        {
+            if (settings->overrideNtsc)
+                globals.isNtsc = settings->isNtsc;
+            if (settings->overrideSidModel)
+                globals.isSidModel8580 = settings->isSidModel8580;
+            if (settings->overrideFiltering)
+                globals.isFiltering = settings->isFiltering;
+            if (settings->overrideDistortion)
+                globals.isDistortion = settings->isDistortion;
+            if (settings->overrideMultiplier)
+                globals.multiplier = settings->multiplier;
+            if (settings->overrideFineVibrato)
+                globals.fineVibrato = settings->fineVibrato;
+            if (settings->overridePulseOptimization)
+                globals.pulseOptimization = settings->pulseOptimization;
+            if (settings->overrideEffectOptimization)
+                globals.effectOptimization = settings->effectOptimization;
+            if (settings->overrideSurround)
+                globals.surround = settings->surround;
+            if (settings->overrideHardRestart)
+                globals.hardRestart = settings->hardRestart;
+        }
+        multiplier = globals.multiplier;
+        finevibrato = globals.fineVibrato;
+        optimizepulse = globals.pulseOptimization;
+        optimizerealtime = globals.effectOptimization;
+        adparam = globals.hardRestart;
+
         initchannels();
         clearsong(1, 1, 1, 1, 1);
 
@@ -177,16 +214,12 @@ namespace rePlayer
                 songs++;
         }
 
-        auto* globals = static_cast<Globals*>(g_replayPlugin.globals);
-        auto* settings = metadata.Find<Settings>();
-
-        bool isNtsc = (settings && settings->overrideNtsc) ? settings->isNtsc : globals->isNtsc;
         sid_init(kSampleRate
-            , (settings && settings->overrideSidModel) ? settings->isSidModel8580 : globals->isSidModel8580
-            , isNtsc
-            , (settings && settings->overrideFiltering) ? settings->isFiltering : globals->isFiltering
+            , globals.isSidModel8580
+            , globals.isNtsc
+            , globals.isFiltering
             , 0
-            , (settings && settings->overrideDistortion) ? settings->isDistortion : globals->isDistortion);
+            , globals.isDistortion);
         for (uint32_t i = 0; i < 16384; ++i)
             playroutine();
 
@@ -195,7 +228,7 @@ namespace rePlayer
         stream->Seek(0, io::Stream::kSeekBegin);
         uint32_t songId;
         stream->Read(&songId, sizeof(songId));
-        return new ReplayGoatTracker(songs, songId, isNtsc);
+        return new ReplayGoatTracker(songs, songId, globals, stream->GetName());
     }
 
     bool ReplayGoatTracker::DisplaySettings()
@@ -205,34 +238,41 @@ namespace rePlayer
             const char* const sidModels[] = { "6581", "8580" };
             int index = ms_globals.isSidModel8580 ? 1 : 0;
             changed |= ImGui::Combo("SID Default Model", &index, sidModels, NumItemsOf(sidModels));
-            if (ImGui::IsItemHovered())
-                ImGui::Tooltip("Applied only on reload :(");
             ms_globals.isSidModel8580 = index == 1;
         }
         {
             const char* const clocks[] = { "PAL", "NTSC" };
             auto index = ms_globals.isNtsc ? 1 : 0;
             changed |= ImGui::Combo("Default Clock###SidClock", &index, clocks, NumItemsOf(clocks));
-            if (ImGui::IsItemHovered())
-                ImGui::Tooltip("Applied only on reload :(");
             ms_globals.isNtsc = index == 1;
         }
+        const char* const onOff[] = { "off", "on" };
         {
-            const char* const filters[] = { "off", "on" };
             auto index = ms_globals.isFiltering ? 1 : 0;
-            changed |= ImGui::Combo("Filtering", &index, filters, NumItemsOf(filters));
-            if (ImGui::IsItemHovered())
-                ImGui::Tooltip("Applied only on reload :(");
+            changed |= ImGui::Combo("Filtering", &index, onOff, NumItemsOf(onOff));
             ms_globals.isFiltering = index == 1;
         }
         {
-            const char* const distortions[] = { "off", "on" };
             auto index = ms_globals.isDistortion ? 1 : 0;
-            changed |= ImGui::Combo("Distortion", &index, distortions, NumItemsOf(distortions));
-            if (ImGui::IsItemHovered())
-                ImGui::Tooltip("Applied only on reload :(");
+            changed |= ImGui::Combo("Distortion", &index, onOff, NumItemsOf(onOff));
             ms_globals.isDistortion = index == 1;
         }
+        {
+            auto index = ms_globals.fineVibrato ? 1 : 0;
+            changed |= ImGui::Combo("Fine Vibrato", &index, onOff, NumItemsOf(onOff));
+            ms_globals.fineVibrato = index == 1;
+        }
+        {
+            auto index = ms_globals.pulseOptimization ? 1 : 0;
+            changed |= ImGui::Combo("Pulse Optimization", &index, onOff, NumItemsOf(onOff));
+            ms_globals.pulseOptimization = index == 1;
+        }
+        {
+            auto index = ms_globals.effectOptimization ? 1 : 0;
+            changed |= ImGui::Combo("Effect Optimization", &index, onOff, NumItemsOf(onOff));
+            ms_globals.effectOptimization = index == 1;
+        }
+        changed |= ImGui::SliderInt("Hard Restart", reinterpret_cast<int32_t*>(&ms_globals.hardRestart), 0, 65535, "%04X", ImGuiSliderFlags_NoInput);
         {
             const char* const surround[] = { "Default", "Surround" };
             changed |= ImGui::Combo("Output", &ms_globals.surround, surround, NumItemsOf(surround));
@@ -249,21 +289,39 @@ namespace rePlayer
             ms_globals.isSidModel8580, "Sid Model: 6581", "Sid Model: 8580");
         ComboOverride("SidClock", GETSET(entry, overrideNtsc), GETSET(entry, isNtsc),
             ms_globals.isNtsc, "Clock Speed: PAL", "Clock Speed: NTSC");
+        ComboOverride("ClockScale", GETSET(entry, overrideMultiplier), GETSET(entry, multiplier),
+            ms_globals.multiplier, "Clock Scale: x0.5", "Clock Scale: x1", "Clock Scale: x2", "Clock Scale: x3", "Clock Scale: x4"
+            , "Clock Scale: x5", "Clock Scale: x6", "Clock Scale: x7", "Clock Scale: x8"
+            , "Clock Scale: x9", "Clock Scale: x10", "Clock Scale: x11", "Clock Scale: x12"
+            , "Clock Scale: x13", "Clock Scale: x14", "Clock Scale: x15", "Clock Scale: x16");
         ComboOverride("Filtering", GETSET(entry, overrideFiltering), GETSET(entry, isFiltering),
             ms_globals.isFiltering, "Filtering: off", "Filtering: on");
         ComboOverride("Distortion", GETSET(entry, overrideDistortion), GETSET(entry, isDistortion),
             ms_globals.isDistortion, "Distortion: off", "Distortion: on");
+        ComboOverride("FineVibrato", GETSET(entry, overrideFineVibrato), GETSET(entry, fineVibrato),
+            ms_globals.fineVibrato, "Fine Vibrato: off", "Fine Vibrato: on");
+        ComboOverride("PulseOptim", GETSET(entry, overridePulseOptimization), GETSET(entry, pulseOptimization),
+            ms_globals.pulseOptimization, "Pulse Optimization: off", "Pulse Optimization: on");
+        ComboOverride("EffectOptim", GETSET(entry, overrideEffectOptimization), GETSET(entry, effectOptimization),
+            ms_globals.effectOptimization, "Effect Optimization: off", "Effect Optimization: on");
+        SliderOverride("HardRestart", GETSET(entry, overrideHardRestart), GETSET(entry, hardRestart),
+            ms_globals.hardRestart, 0, 65535, "Hard Restart %04X");
         ComboOverride("Output", GETSET(entry, overrideSurround), GETSET(entry, surround),
             ms_globals.surround, "Output: Default", "Output: Surround");
 
-        context.metadata.Update(entry, entry->value == 0);
+        context.metadata.Update(entry, entry->value == 0 && entry->value2 == 0);
     }
 
     ReplayGoatTracker::Globals ReplayGoatTracker::ms_globals = {
-        .isSidModel8580 = 0,
-        .isNtsc = 0,
-        .isFiltering = 0,
-        .isDistortion = 0,
+        .isSidModel8580 = false,
+        .isNtsc = false,
+        .isFiltering = false,
+        .isDistortion = false,
+        .multiplier = 1,
+        .fineVibrato = true,
+        .pulseOptimization = false,
+        .effectOptimization = false,
+        .hardRestart = 0x0f00,
         .surround = 1
     };
 
@@ -272,12 +330,15 @@ namespace rePlayer
         sid_uninit();
     }
 
-    ReplayGoatTracker::ReplayGoatTracker(uint32_t numSongs, uint32_t songId, bool isNtsc)
+    ReplayGoatTracker::ReplayGoatTracker(uint32_t numSongs, uint32_t songId, Globals& globals, const std::string& name)
         : Replay(eExtension::_sng, eReplay::GoatTracker)
         , m_surround(kSampleRate)
         , m_numSongs(numSongs)
         , m_songId(songId)
-        , m_tempo(isNtsc ? 150 : 125)
+        , m_name(name)
+        , m_tempo(globals.isNtsc ? 150 : 125)
+        , m_currentSettings(globals)
+        , m_updatedSettings(globals)
     {}
 
     struct Sequence
@@ -293,6 +354,28 @@ namespace rePlayer
             m_hasLooped = false;
             return 0;
         }
+
+        auto settings = m_updatedSettings;
+        if (settings.isSidModel8580 != m_currentSettings.isSidModel8580 ||
+            settings.isNtsc != m_currentSettings.isNtsc ||
+            settings.isFiltering != m_currentSettings.isFiltering ||
+            settings.isDistortion != m_currentSettings.isDistortion)
+        {
+            sid_uninit();
+            sid_init(kSampleRate
+                , settings.isSidModel8580
+                , settings.isNtsc
+                , settings.isFiltering
+                , 0
+                , settings.isDistortion);
+            m_tempo = settings.isNtsc ? 150 : 125;
+            m_currentSettings = settings;
+        }
+        multiplier = settings.multiplier;
+        finevibrato = settings.fineVibrato;
+        optimizepulse = settings.pulseOptimization;
+        optimizerealtime = settings.effectOptimization;
+        adparam = settings.hardRestart;
 
         auto* buffer = reinterpret_cast<int16_t*>(output + numSamples) - numSamples * 2;
 
@@ -318,14 +401,18 @@ namespace rePlayer
 
                 playroutine();
 
-                m_remainingSamples = ((kSampleRate * 5) >> 1) / m_tempo;
+                if (settings.multiplier)
+                    m_remainingSamples = ((kSampleRate * 5) >> 1) / m_tempo / settings.multiplier;
+                else
+                    m_remainingSamples = ((kSampleRate * 5) >> 1) * 2 / m_tempo;
 
-                if (hasPatBreak && !getPatBreak())
+                if ((hasPatBreak && !getPatBreak()) || songinit == PLAY_STOP)
                 {
+                    m_hasFailed = songinit == PLAY_STOP;
                     uint32_t songsPtr = 0;
                     for (uint32_t i = 0; i < MAX_CHN; ++i)
                         songsPtr |= uint32_t(chn[i].songptr) << (i * 8);
-                    if (m_sequence.FindIf([songsPtr](auto& entry)
+                    if (songinit == PLAY_STOP || m_sequence.FindIf([songsPtr](auto& entry)
                     {
                         if (entry.songsPtr == songsPtr)
                         {
@@ -374,6 +461,8 @@ namespace rePlayer
         for (uint32_t i = 0; i < 16384; ++i)
             playroutine();
 
+        m_hasFailed = m_hasNoticedFailure = false;
+
         int subsongIndex = 0;
         for (uint32_t songs = 0; subsongIndex < MAX_SONGS; subsongIndex++)
         {
@@ -396,9 +485,32 @@ namespace rePlayer
 
     void ReplayGoatTracker::ApplySettings(const CommandBuffer metadata)
     {
-        auto* globals = static_cast<Globals*>(g_replayPlugin.globals);
-        auto* settings = metadata.Find<Settings>();
-        m_surround.Enable((settings && settings->overrideSurround) ? settings->surround : globals->surround);
+        auto globals = *static_cast<Globals*>(g_replayPlugin.globals);
+        if (auto* settings = metadata.Find<Settings>())
+        {
+            if (settings->overrideNtsc)
+                globals.isNtsc = settings->isNtsc;
+            if (settings->overrideSidModel)
+                globals.isSidModel8580 = settings->isSidModel8580;
+            if (settings->overrideFiltering)
+                globals.isFiltering = settings->isFiltering;
+            if (settings->overrideDistortion)
+                globals.isDistortion = settings->isDistortion;
+            if (settings->overrideMultiplier)
+                globals.multiplier = settings->multiplier;
+            if (settings->overrideFineVibrato)
+                globals.fineVibrato = settings->fineVibrato;
+            if (settings->overridePulseOptimization)
+                globals.pulseOptimization = settings->pulseOptimization;
+            if (settings->overrideEffectOptimization)
+                globals.effectOptimization = settings->effectOptimization;
+            if (settings->overrideSurround)
+                globals.surround = settings->surround;
+            if (settings->overrideHardRestart)
+                globals.hardRestart = settings->hardRestart;
+        }
+        m_surround.Enable(globals.surround);
+        m_updatedSettings = globals;
     }
 
     void ReplayGoatTracker::SetSubsong(uint32_t subsongIndex)
@@ -434,7 +546,26 @@ namespace rePlayer
         std::string info;
         info = "3 channels\n";
         info.append(reinterpret_cast<const char*>(&m_songId), 4);
+        if (m_updatedSettings.multiplier != 1)
+        {
+            info += " clock x";
+            if (m_updatedSettings.multiplier == 0)
+                info += "0.5";
+            else
+            {
+                char txt[3];
+                _ultoa_s(m_updatedSettings.multiplier, txt, 10);
+                info += txt;
+            }
+        }
         info += "\nGoatTracker " GoatTrackerVersion;
+
+        if (!m_hasNoticedFailure && m_hasFailed)
+        {
+            m_hasNoticedFailure = true;
+            Log::Error("GoatTracker: playback issue on subsong %u for \"%s\" (try changing the clock scale)\n", m_subsongIndex + 1, m_name.c_str());
+        }
+
         return info;
     }
 }
--- source/Replays/GoatTracker/ReplayGoatTracker.h
+++ source/Replays/GoatTracker/ReplayGoatTracker.h
@@ -29,9 +29,26 @@ namespace rePlayer
                     uint32_t isFiltering : 1;
                     uint32_t overrideDistortion : 1;
                     uint32_t isDistortion : 1;
+                    uint32_t overrideMultiplier : 1;
+                    uint32_t multiplier : 5;
+                    uint32_t overrideFineVibrato : 1;
+                    uint32_t fineVibrato : 1;
+                    uint32_t overridePulseOptimization : 1;
+                    uint32_t pulseOptimization : 1;
+                    uint32_t overrideEffectOptimization : 1;
+                    uint32_t effectOptimization : 1;
 
                     uint32_t overrideSurround : 1;
                     uint32_t surround : 1;
+                };
+            };
+            union
+            {
+                uint32_t value2 = 0;
+                struct
+                {
+                    uint32_t overrideHardRestart : 1;
+                    uint32_t hardRestart : 16;
                 };
             };
 
@@ -59,29 +76,43 @@ namespace rePlayer
     private:
         static constexpr uint32_t kSampleRate = 48000;
 
-    private:
-        ReplayGoatTracker(uint32_t numSongs, uint32_t songId, bool isNtsc);
-
-    private:
-        Surround m_surround;
-        const uint32_t m_numSongs;
-        const uint32_t m_songId;
-
-        const uint32_t m_tempo;
-        uint32_t m_remainingSamples = 0;
-        bool m_hasLooped = false;
-
-        Array<struct Sequence> m_sequence;
-
-    public:
         struct Globals
         {
             bool isSidModel8580;
             bool isNtsc;
             bool isFiltering;
             bool isDistortion;
+            uint8_t multiplier;
+            bool fineVibrato;
+            bool pulseOptimization;
+            bool effectOptimization;
+            uint32_t hardRestart;
             int32_t surround;
-        } static ms_globals;
+        };
+
+    private:
+        ReplayGoatTracker(uint32_t numSongs, uint32_t songId, Globals& globals, const std::string& name);
+
+    private:
+        Surround m_surround;
+        const uint32_t m_numSongs;
+        const uint32_t m_songId;
+        std::string m_name;
+
+        uint32_t m_tempo;
+        uint32_t m_remainingSamples = 0;
+        bool m_hasLooped = false;
+        bool m_hasFailed = false;
+        mutable bool m_hasNoticedFailure = false;
+
+        Array<struct Sequence> m_sequence;
+
+    public:
+        static Globals ms_globals;
+
+    private:
+        Globals m_currentSettings;
+        Globals m_updatedSettings;
     };
 }
 // namespace rePlayer
