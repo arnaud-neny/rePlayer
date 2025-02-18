93390a0f51ecb42b93b42de48ee422b9b27bdd54
replay <mr.arnal@gmail.com>
2025-02-18 17:38:14 +0000

    libxml2: libxml 2.13.6

--- source/3rdParty/libxml2/config.h
+++ source/3rdParty/libxml2/config.h
@@ -30,20 +30,11 @@
 /* Define to 1 if you have the `gettimeofday' function. */
 #undef HAVE_GETTIMEOFDAY
 
-/* Define to 1 if you have the <glob.h> header file. */
-#undef HAVE_GLOB_H
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
-
 /* Define if history library is there (-lhistory) */
 #undef HAVE_LIBHISTORY
 
 /* Define if readline library is there (-lreadline) */
 #undef HAVE_LIBREADLINE
-
-/* Define to 1 if you have the <lzma.h> header file. */
-#undef HAVE_LZMA_H
 
 /* Define to 1 if you have the `mmap' function. */
 #undef HAVE_MMAP
@@ -77,18 +68,6 @@
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
 
-/* Define to 1 if you have the <stdio.h> header file. */
-#define HAVE_STDIO_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
 /* Define to 1 if you have the <sys/mman.h> header file. */
 #undef HAVE_SYS_MMAN_H
 
@@ -110,43 +89,11 @@
 /* Define to 1 if you have the <sys/time.h> header file. */
 #undef HAVE_SYS_TIME_H
 
-/* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
-
 /* Define to 1 if you have the <unistd.h> header file. */
 #undef HAVE_UNISTD_H
 
 /* Define to 1 if you have the <zlib.h> header file. */
 #undef HAVE_ZLIB_H
-
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#undef LT_OBJDIR
-
-/* Name of package */
-#define PACKAGE "libxml2"
-
-/* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "libxml2"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libxml2 2.13.5"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libxml2"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL "https://gitlab.gnome.org/GNOME/libxml2"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "2.13.5"
-
-/* Define to 1 if all of the C90 standard headers exist (not just the ones
-   required in a freestanding environment). This macro is provided for
-   backward compatibility; new code need not use it. */
-#undef STDC_HEADERS
 
 /* Support for IPv6 */
 #undef SUPPORT_IP6
