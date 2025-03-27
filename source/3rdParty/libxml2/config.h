994cfa81385e2a97be1ffd2d0ca7186e23f91cd7
replay <mr.arnal@gmail.com>
2025-03-27 17:54:49 +0000

    libxml2: libxml 2.14.0

--- source/3rdParty/libxml2/config.h
+++ source/3rdParty/libxml2/config.h
@@ -1,34 +1,20 @@
-/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* Define to 1 if you have the declaration of 'getentropy', and to 0 if you
+   don't. */
+#define HAVE_DECL_GETENTROPY 0
 
-/* A form that will not confuse apibuild.py */
-#undef ATTRIBUTE_DESTRUCTOR
+/* Define to 1 if you have the declaration of 'glob', and to 0 if you don't.
+   */
+#define HAVE_DECL_GLOB 0
 
-/* Define to 1 if you have the <arpa/inet.h> header file. */
-#undef HAVE_ARPA_INET_H
+/* Define to 1 if you have the declaration of 'mmap', and to 0 if you don't.
+   */
+#define HAVE_DECL_MMAP 0
 
 /* Define if __attribute__((destructor)) is accepted */
-#undef HAVE_ATTRIBUTE_DESTRUCTOR
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+#undef HAVE_FUNC_ATTRIBUTE_DESTRUCTOR
 
 /* Have dlopen based dso */
 #undef HAVE_DLOPEN
-
-/* Define to 1 if you have the <dl.h> header file. */
-#undef HAVE_DL_H
-
-/* Define to 1 if you have the <fcntl.h> header file. */
-#define HAVE_FCNTL_H 1
-
-/* Define to 1 if you have the `ftime' function. */
-#undef HAVE_FTIME
-
-/* Define to 1 if you have the `getentropy' function. */
-#undef HAVE_GETENTROPY
-
-/* Define to 1 if you have the `gettimeofday' function. */
-#undef HAVE_GETTIMEOFDAY
 
 /* Define if history library is there (-lhistory) */
 #undef HAVE_LIBHISTORY
@@ -36,73 +22,17 @@
 /* Define if readline library is there (-lreadline) */
 #undef HAVE_LIBREADLINE
 
-/* Define to 1 if you have the `mmap' function. */
-#undef HAVE_MMAP
-
-/* Define to 1 if you have the `munmap' function. */
-#undef HAVE_MUNMAP
-
-/* mmap() is no good without munmap() */
-#if defined(HAVE_MMAP) && !defined(HAVE_MUNMAP)
-#  undef /**/ HAVE_MMAP
-#endif
-
-/* Define to 1 if you have the <netdb.h> header file. */
-#undef HAVE_NETDB_H
-
-/* Define to 1 if you have the <netinet/in.h> header file. */
-#undef HAVE_NETINET_IN_H
-
 /* Define to 1 if you have the <poll.h> header file. */
-#undef HAVE_POLL_H
-
-/* Define to 1 if you have the <pthread.h> header file. */
-#undef HAVE_PTHREAD_H
+#define HAVE_POLL_H 0
 
 /* Have shl_load based dso */
 #undef HAVE_SHLLOAD
 
-/* Define to 1 if you have the `stat' function. */
-#undef HAVE_STAT
-
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
 
-/* Define to 1 if you have the <sys/mman.h> header file. */
-#undef HAVE_SYS_MMAN_H
-
-/* Define to 1 if you have the <sys/random.h> header file. */
-#undef HAVE_SYS_RANDOM_H
-
-/* Define to 1 if you have the <sys/select.h> header file. */
-#undef HAVE_SYS_SELECT_H
-
-/* Define to 1 if you have the <sys/socket.h> header file. */
-#undef HAVE_SYS_SOCKET_H
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
-
-/* Define to 1 if you have the <sys/timeb.h> header file. */
-#undef HAVE_SYS_TIMEB_H
-
-/* Define to 1 if you have the <sys/time.h> header file. */
-#undef HAVE_SYS_TIME_H
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
-
-/* Define to 1 if you have the <zlib.h> header file. */
-#undef HAVE_ZLIB_H
-
-/* Support for IPv6 */
-#undef SUPPORT_IP6
-
-/* Version number of package */
-#undef VERSION
-
-/* Determine what socket length (socklen_t) data type is */
-#undef XML_SOCKLEN_T
+/* System configuration directory (/etc) */
+#define XML_SYSCONFDIR ""
 
 /* TLS specifier */
 #undef XML_THREAD_LOCAL
