1eec43ce0356af94299cede9e9ed5fcc45c12014
replay <mr.arnal@gmail.com>
2026-09-19 14:04:39 +0100

    fluidsynth: fluidsynth 2.6.1

--- source/Replays/FluidSynth/fluidsynth/include/fluidsynth/version.h
+++ source/Replays/FluidSynth/fluidsynth/include/fluidsynth/version.h
@@ -30,10 +30,10 @@ extern "C" {
  *
  * @{
  */
-#define FLUIDSYNTH_VERSION       "2.6.0"         /**< String constant of libfluidsynth version. */
+#define FLUIDSYNTH_VERSION       "2.6.1"         /**< String constant of libfluidsynth version. */
 #define FLUIDSYNTH_VERSION_MAJOR 2     /**< libfluidsynth major version integer constant. */
 #define FLUIDSYNTH_VERSION_MINOR 6     /**< libfluidsynth minor version integer constant. */
-#define FLUIDSYNTH_VERSION_MICRO 0     /**< libfluidsynth micro version integer constant. */
+#define FLUIDSYNTH_VERSION_MICRO 1     /**< libfluidsynth micro version integer constant. */
 
 FLUIDSYNTH_API void fluid_version(int *major, int *minor, int *micro);
 FLUIDSYNTH_API const char* fluid_version_str(void);
